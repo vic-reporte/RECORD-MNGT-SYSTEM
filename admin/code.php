b7df52e4c381dab872f9f56cce918eb3250bda5c
vic-reporte <vic1984reporte@gmail.com>
2024-09-21 21:29:38 +0800

    record-category.php

---
+++ admin/code.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST['record_category_btn']))
+{
+    $route_number = $_POST['route_number'];
+    $record_type = $_POST['record_type'];
+    $source = $_POST['source'];
+    $subject_matter = $_POST['subject_matter'];
+    $action_unit = $_POST['route_name'];
+    $status = $_POST['status'];
+    $remark = $_POST['remark'];
+}
+
+?>
--- admin/record-category.php
+++ admin/record-category.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
-include('includes/header.php'); 
-include('../middleware/adminMiddleware.php'); 
+include('includes/header.php');
+include('../middleware/adminMiddleware.php');
 ?>
 
 <div class="container-fluid py-4">
@@ -12,8 +12,45 @@ include('../middleware/adminMiddleware.php');
                     <h4>Record Category</h4>
                 </div>
                 <div class="card-body">
-                    <label for="">Control Number</label>
-                    <input type="text" class="form-control">
+                    <form action="code.php" method="POST" enctype="multipart/form-data">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label for="">Route Number</label>
+                                <input type="text" name="route_number" placeholder="Enter Route Number" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Record Type</label>
+                                <input type="text" name="record type" placeholder="Enter Record Type" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Source</label>
+                                <input type="text" name="source" placeholder="Enter Source" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Subject Matter</label>
+                                <input type="textarea" name="subject_matter" rows="5" cols="20" placeholder="Enter Subject Matter" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Action Unit</label>
+                                <input type="text" name="action_unit" placeholder="Enter Action Unit" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Status</label>
+                                <input type="text" name="status" placeholder="Enter Status" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Remark</label>
+                                <input type="text" name="remark" placeholder="Enter Remark" class="form-control">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="">Upload Image</label>
+                                <input type="file" name="image" class="form-control">
+                            </div>
+                            <div clas="col-md-12">
+                                <button type="submit" class="btn btn-primary" name="record_category_btn">Save</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
