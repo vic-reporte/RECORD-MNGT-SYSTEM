c6b6b3939fef2c7fee7d7aaeabbc417cacb0e237
vic-reporte <vic1984reporte@gmail.com>
2024-10-03 23:08:56 +0800

    Update admin/includes/sidebar.php and add admin/category.php

---
+++ admin/category.php
@@ -0,0 +1,27 @@
+<?php
+
+include('includes/header.php');
+include('../middleware/adminMiddleware.php');
+?>
+
+<div class="container-fluid py-4">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+              <div class="card-header">
+                Categories
+              </div>
+              <div class="card-body">
+                <table class="table table-boardered">
+                   <thead>
+                    <tr></tr>
+                   </thead>
+
+                </table>
+              </div>  
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
